e51907942be77871c380b02922935520e64d1020
Matija Štrlekar <mstrlekar@gmail.com>
2023-09-24 15:59:10 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Product, ProductsResponse } from './product';
+import { AppService } from './app.service';
+import { TableLazyLoadEvent } from 'primeng/table';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-1';
+  products: Product[] = [];
+  totalRecords: number = 0;
+
+  // TODO - id and sort is not working
+
+  sortField: string = 'id';
+  sortOrder: number = 1;
+
+  // implement sortField and sortOrderđ
+
+  loader: boolean = true;
+
+  constructor(private appService: AppService) {}
+
+  // ngOnInit() {
+  //   this.loadProducts();
+  // }
+
+  loadProducts($event: TableLazyLoadEvent) {
+    this.loader = true;
+
+
+    this.appService.getProducts($event.first || 0).subscribe((response) => {
+      this.loader = false;
+      this.products = response.products;
+      this.totalRecords = response.total;
+    });
+  }
+
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductsResponse } from './product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) {}
+
+    getProducts(skip: number): Observable<ProductsResponse> {
+
+      return this.http.get<ProductsResponse>(`https://dummyjson.com/products?limit=10&skip=${skip}`);
+    }
+
+}
